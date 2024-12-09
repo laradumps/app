6a638c82818174b7d8323e0f05c160a9f2e8ac1b
luanfreitasdev <luanfreitas10@protonmail.com>
2024-12-09 17:24:26 -0300

    Remove unused import

--- src/renderer/components/DumpDump.vue
+++ src/renderer/components/DumpDump.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps, onMounted } from "vue";
+import { defineProps } from "vue";
 import { Payload } from "@/types/Payload";
 import VueJsonPretty from "vue-json-pretty";
 
