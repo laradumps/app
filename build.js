4e32a370e30b34bc531e8742e18ed00375a5f68c
luanfreitasdev <luanfreitas10@protonmail.com>
2025-01-14 13:00:32 -0300

    wip

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ const { copyFileSync } = require("fs");
 const options = {
     platform: "node",
     bundle: true,
-    external: ["electron", "cpu-features"],
+    external: ["electron"],
     define: {
         "process.env.NODE_ENV": `"${process.argv[2] === "--dev" ? "development" : "production"}"`,
         "process.platform": `"${process.platform}"`
