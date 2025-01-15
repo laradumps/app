63baad0ad3ddf408b7b6c9fed7ec53b1249472aa
luanfreitasdev <luanfreitas10@protonmail.com>
2025-01-15 07:13:26 -0300

    wip

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ const options = {
     platform: "node",
     bundle: true,
     target: 'node20',
-    external: ["electron"],
+    external: ["electron", "cpu-features"],
     define: {
         "process.env.NODE_ENV": `"${process.argv[2] === "--dev" ? "development" : "production"}"`,
         "process.platform": `"${process.platform}"`
