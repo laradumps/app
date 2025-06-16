c9f99cf8e660befff246dea89391ab40ad5b3f76
luanfreitasdev <luanfreitas10@protonmail.com>
2025-06-16 11:09:49 -0300

    fix: update server icon usage and bump version to 4.4.1

--- src/renderer/components/NavBarSSH.vue
+++ src/renderer/components/NavBarSSH.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import { ArrowPathIcon, PencilIcon, ServerIcon, TrashIcon } from "@heroicons/vue/24/outline";
-import { ServerIcon as ServerIconOutline } from "@heroicons/vue/24/outline";
+import { ArrowPathIcon, PencilIcon, TrashIcon } from "@heroicons/vue/24/outline";
+import { ServerIcon } from "@heroicons/vue/24/outline";
 
 import { useSSHStore } from "@/store/ssh";
 import Modal from "./Modal.vue";
@@ -153,11 +153,12 @@ const editConnection = (id: number) => {
             :title="$t('menu.ssh')"
             class="p-2 hover:bg-base-200 text-base-content cursor-pointer rounded-md"
         >
-            <ServerIconOutline
-                v-if="connected"
-                class="w-4 text-primary"
+            <ServerIcon
+                class="w-4"
+                :class="{
+                    'text-primary': connected,
+                }"
             />
-            <ServerIcon class="w-4" />
         </button>
 
         <ul
