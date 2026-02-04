dfea71dfcb7aa5f6ef7ee619e9eb2500b8e0c123
luanfreitasdev <luanfreitas10@protonmail.com>
2026-02-04 11:15:58 -0300

    feat: simplify MCP indicator animation in TheNavBar component

--- src/renderer/components/navbar/TheNavBar.vue
+++ src/renderer/components/navbar/TheNavBar.vue
@@ -105,9 +105,7 @@ const modalClose = () => (isListeningModalOpen.value = false);
                 @click="router.push('/settings?tab=mcp')"
             >
                 <span class="relative flex h-2 w-2">
-                    <span
-                        class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"
-                    ></span>
+                    <span class="absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                     <span class="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
                 </span>
                 <span class="text-[10px] uppercase font-bold text-base-content/70">MCP</span>
